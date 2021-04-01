3d51117fe733d022f4aa17eb30f5e8c774642594
Miguel Benalcazar <migangben@gmail.com>
2021-04-01 12:52:13 +0800

    page 34 softmax

---
+++ CH4/SoftmaxActivationFunction_p32.py
@@ -0,0 +1,22 @@
+import math
+
+# values from the previous output when we describe
+# what a neural network is
+layer_outputs = [4.8, 1.21, 2.385]
+
+# e - mathematical constant, we use E here to match a common coding
+# style where constants are upercase
+
+E = math.e
+
+# for each value in a vector, calculate the exponential value
+exp_values = []
+for output in layer_outputs:
+    exp_values.append(E ** output) # ** - power operator in Python
+
+print('exponentiated values: {}'.format(exp_values))
+
+
+#normalize values
+norm_base = sum(exp_values)
+norm_values = []
