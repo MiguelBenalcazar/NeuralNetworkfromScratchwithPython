61d92128797846c70f7ddde4c443e0d58bf96e61
blackz2 <blackz2.com@gmail.com>
2021-04-07 12:58:10 +0800

    Last Code Chapter 4

--- CH4/SoftmaxClass_p38.py
+++ CH4/SoftmaxClass_p38.py
@@ -12,3 +12,20 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1,keepdims=True)
         self.output = probabilities
 
+
+x = [[1, 2, 3]]
+softmax = Activation_Softmax()
+softmax.forward(x)
+out_x = softmax.output
+print( "softmax({}) = {}".format(x,out_x))
+
+y = [[-2, -1, 0]]
+softmax.forward(y)
+out_y  = softmax.output
+print( "softmax({}) = {}".format(y,out_y))
+
+z = np.divide(x,2)
+softmax.forward(z)
+out_z = softmax.output
+print( "softmax({}) = {}".format(z,out_z))
+
