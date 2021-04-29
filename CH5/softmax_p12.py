3a72dc6b48ef06e665180c795fe55440a28f7ade
blackz2 <blackz2.com@gmail.com>
2021-04-29 17:57:53 +0800

    softmax CHAPTER 5 page 15

--- CH5/softmax_p12.py
+++ CH5/softmax_p12.py
@@ -13,4 +13,14 @@ softmax_outputs = np.array([[0.7, 0.1, 0.2],
                    [0.02, 0.9, 0.08]])
 
 class_targets = [0, 1, 1] #dog, cat, cat
-print(softmax_outputs[[0,1,2], class_targets])
+
+print(softmax_outputs[[0, 1, 2], class_targets])
+
+print(softmax_outputs[range(len(softmax_outputs)), class_targets])
+
+print(-np.log(softmax_outputs[range(len(softmax_outputs)), class_targets]))
+
+neg_log = -np.log(softmax_outputs[range(len(softmax_outputs)), class_targets])
+
+average_loss = np.mean(neg_log)
+print(average_loss)
